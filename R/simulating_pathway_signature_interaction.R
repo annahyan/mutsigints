f937ce85c874ed28ac03318fa9da1a49a579ecc2
Anna <anna@admins-Air.fritz.box>
2022-06-26 10:16:51 +0200

    simulation merge

--- R/simulating_pathway_signature_interaction.R
+++ R/simulating_pathway_signature_interaction.R
@@ -88,6 +88,8 @@ log.out = get_sig_path_lms(sim.out[, "signature", drop = FALSE],
                                   path.to.sig = FALSE)
 
 
+#### Simple simulations
+
 lm.sim = lm(signature ~ pathway, data = sim.out)
 lm.sim.summary = summary(lm.sim)
 
@@ -111,3 +113,57 @@ x.y = lm(x ~ y)
 summary(x.y)$coefficients
 
 
+
+### P-value based distinction between two regression types
+    
+N = 100
+r = 0.7
+
+mu1 = 300
+mu2 = 300
+zero.p1 = 0.6
+zero.p2 = 0.6
+
+lm.coeffs = c()
+lm.pvals = c()
+log.coeffs = c()
+log.pvals = c()
+
+zero.p1.seq =  seq(0.1, 0.9, 0.1)
+
+for (zero.p1 in zero.p1.seq) {
+   cat(zero.p1)
+     sim.out = simulate_exp(N = 100, r = r, mu1 = 300, 
+                            mu2 = 300, zero.p1 = zero.p1 , zero.p2 = 0.6)
+    
+     lm.out = lm(sim.out[, "signature"] ~ sim.out[, "pathway"])
+     lm.coeffs = c(lm.coeffs, lm.out$coefficients[-1])
+     lm.pvals = c(lm.pvals, summary(lm.out)$coefficients[-1, 4])
+     
+     log.out = glm(sim.out[, "pathway"] ~ sim.out[, "signature"] , family = binomial)
+     log.coeffs = c(log.coeffs, log.out$coefficients[-1])
+     log.pvals = c(log.pvals, summary(log.out)$coefficients[-1, 4])
+}
+
+
+summ.df = rbind(
+    data.frame(var = zero.p1.seq, coeffs = lm.coeffs, pvals = lm.pvals, test = "lm"), 
+    data.frame(var = zero.p1.seq,coeffs = log.coeffs * 5000, pvals = log.pvals, test = "log"))
+
+
+p1.mods.plot =summ.df %>% 
+    mutate() %>% 
+    ggplot(aes(x = var, y = coeffs, color = -log10(pvals)), group = test) +
+    geom_point(aes(shape = test), size = 2) + 
+    scale_y_continuous(
+        # Features of the first axis
+        name = "Linear coefficients",
+        # Add a second axis and specify its features
+        sec.axis = sec_axis(~./5000, name="Logistic coefficients")
+    ) + theme_bw(base_size = 13) + 
+    scale_x_continuous(breaks = zero.p1.seq) +
+    xlab("p1") + ggtitle(paste0('r = ', r))
+
+
+ggsave(filename = here("figures/regression_test/", paste0("p1.mod.r", r, ".png")),
+       width = 5, height = 4)
