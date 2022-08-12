c9becbe991d67168bf72dc0db456aaf6ec6596a1
annahyan <anna@menchelab.com>
2022-08-12 16:23:16 +0200

    pcawg ssGSEA calculations

--- R/ssGSEA_for_immune_genesets.R
+++ R/ssGSEA_for_immune_genesets.R
@@ -9,9 +9,18 @@ query(eh , 'msigdb')
 ## Sources of data
 ## https://gdc.cancer.gov/about-data/publications/panimmune
 
-msigdb.hs = getMsigdb(org = 'hs', id = 'EZID', version = '7.4')
-msigdb.hs
+## https://www.biorxiv.org/content/10.1101/2020.01.17.910950v1.full.pdf
 
+library(ImmuneSubtypeClassifier)
+data("geneSetSymbols")
+names(genesetsymbols)
+
+immune.gene.lists = genesetsymbols
+
+data.dir = here("data", "expression")
+out.dir = here("data/RDS/PCAWG/immune_states")
+
+### The function borrowed from https://rpubs.com/pranali018/SSGSEA
 ssgsea = function(X, gene_sets, alpha = 0.25, scale = T, norm = F, single = T) {
     row_names = rownames(X)
     num_genes = nrow(X)
@@ -61,3 +70,58 @@ ssgsea = function(X, gene_sets, alpha = 0.25, scale = T, norm = F, single = T) {
     colnames(es) = colnames(X)
     return(es)
 }
+
+pcawg.data = read.delim(file.path(data.dir, 
+                                  "tophat_star_fpkm_uq.v2_aliquot_gl_filtered.tsv"),
+                        check.names = FALSE)
+
+rownames(pcawg.data) = pcawg.data$feature
+pcawg.ensembl = pcawg.data$feature
+pcawg.data$feature = NULL
+pcawg.data = as.matrix(pcawg.data)
+pcawg.data[1:4, 1:4]
+
+library("org.Hs.eg.db") # remember to install it if you don't have it already
+symbols <- mapIds(org.Hs.eg.db, keys = pcawg.data$feature, keytype = "ENSEMBL", column="SYMBOL", )
+
+
+pcawg.median = apply(pcawg.data, MARGIN = 1, median)
+head(pcawg.median)
+pcawg.mean = apply(pcawg.data, MARGIN = 1, mean)
+head(pcawg.mean)
+
+medians.p = pcawg.median %>% 
+    enframe() %>% 
+    ggplot(aes(x = value)) + 
+        geom_histogram(binwidth = 0.1) + 
+        coord_cartesian(x = c(0, 30)) + 
+    ggtitle("Ensembl gene medians")
+
+means.p = pcawg.mean %>% 
+    enframe() %>% 
+    ggplot(aes(x = value)) + 
+    geom_histogram(binwidth = 0.1) + 
+    coord_cartesian(x = c(0, 30)) +
+    ggtitle("Ensembl gene means")
+
+
+pp = ggarrange(means.p, medians.p)
+
+pcawg.exp.prop = rowSums(pcawg.data > 0) / ncol(pcawg.data)
+
+pcawg.filtered = pcawg.data[pcawg.mean > 1, ]
+filtered.symbols <- mapIds(org.Hs.eg.db, 
+                           keys = rownames(pcawg.filtered), keytype = "ENSEMBL", column="SYMBOL", )
+
+pcawg.filtered = pcawg.filtered[!is.na(filtered.symbols), ]
+filtered.symbols = filtered.symbols[ !is.na(filtered.symbols)]
+
+filtered.symbols = filtered.symbols[!duplicated(filtered.symbols)]
+
+pcawg.filtered = pcawg.filtered[ names(filtered.symbols), ]
+rownames(pcawg.filtered) = filtered.symbols
+
+gsva_out = gsva(as.matrix(pcawg.filtered), immune.gene.lists, method = "ssgsea")
+
+
+saveRDS(gsva_out, file = file.path(out.dir, "ssGSEA_pcawg.RDS") )
