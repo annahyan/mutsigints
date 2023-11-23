023cc8e6064279a4c55e2c0928df7b83149e482b
annahyan <ahakobyan@cemm.oeaw.ac.at>
2023-11-23 19:39:26 +0100

    filtering all significant interactions pre plotting

--- R/sig_sig_interactions_PCAWG.R
+++ R/sig_sig_interactions_PCAWG.R
@@ -1,8 +1,9 @@
 library(here)
 source(here("R/load_packages.R"))
+library(openxlsx)
 
 fig.dir = here("figures/sig_sig_analysis")
-data.out.dir = here("data/RDS/TCGA", "sig_sig_ints")
+data.out.dir = here("data/RDS/PCAWG", "sig_sig_ints")
 
 for (dir.path in c(fig.dir, data.out.dir) ) {
     if (! file.exists(dir.path)) {
@@ -12,6 +13,9 @@ for (dir.path in c(fig.dir, data.out.dir) ) {
 
 PCAWG.full.subset.ann = readRDS(here("data", "RDS", "PCAWG", "signatures",
                                      "PCAWG.full.subset.ann.RDS"))
+
+PCAWG.null.dists = readRDS(file.path(here("data", "RDS", "PCAWG", "signatures"),
+                                      "PCAWG.null.model.metrics.dist.RDS"))
 
 ### Cooccurrence
 
@@ -108,3 +112,147 @@ PCAWG.coda.summary.nets = lapply(PCAWG.coda, summarize_calcs )
 saveRDS(PCAWG.coda.summary.nets, 
         file = file.path(data.out.dir, "PCAWG.coda.summary.nets.RDS") )
 
+
+### Writing to supplements
+
+
+out.file = here("supp_data/pcawg_sig_sig_all_metrics_report.xlsx")
+
+
+all.interactions = list(MI = PCAWG.bcmi.summary.nets,
+                        cooccurrence = PCAWG.cooccurrence.summary.nets,
+                        CoDa = PCAWG.coda.summary.nets,
+                        Spearman = PCAWG.spearman.summary.nets)
+
+
+sig.threshold = 0.05
+
+all.sig.interactions = all.interactions
+for(tissue in tissues) {
+    
+    for (metric.name in names(all.interactions)) {
+        cat(tissue, "\n")
+        out =  all.interactions[[metric.name]][[tissue]]
+        metric.values = out %>% rm_zeros()
+        if (length(metric.values) == 0) {next} 
+        
+        p.values = matrix(1, nrow = nrow(metric.values), 
+                          ncol = ncol(metric.values), 
+                          dimnames = (dimnames(metric.values)))
+        
+        metric.values[lower.tri(metric.values, diag = TRUE) ] = 0
+        
+        non.zero.values = which(metric.values != 0, arr.ind = TRUE)
+        
+        for(i in 1:nrow(non.zero.values)) {
+            sig1 = rownames(metric.values)[non.zero.values[i, 1]]
+            sig2 = rownames(metric.values)[non.zero.values[i, 2]]
+            
+            p.values[sig1, sig2] = 1 - percentile(PCAWG.null.dists[[tissue]][[metric.name]][sig1, sig2, ],
+                                                  abs(metric.values[sig1, sig2]))
+            
+            if(p.values[sig1, sig2] > sig.threshold) {
+                out[sig1, sig2] = 0
+                out[sig2, sig1] = 0
+            }
+        }
+        all.sig.interactions[[metric.name]][[tissue]] = out
+    }
+}
+
+
+### Write the metric name
+for (tissue in tissues) {
+    for (metric.name in names(all.sig.interactions)) {
+        
+        cat(tissue, "\n")
+        metric.values = all.sig.interactions[[metric.name]][[tissue]] %>% rm_zeros()
+        
+        if (length(metric.values) == 0) {next} 
+        
+        p.values = matrix(NA, nrow = nrow(metric.values), 
+                          ncol = ncol(metric.values), 
+                          dimnames = (dimnames(metric.values)))
+        
+        metric.values[lower.tri(metric.values, diag = TRUE) ] = NA
+        
+        non.zero.values = which(metric.values != 0, arr.ind = TRUE)
+        
+        for(i in 1:nrow(non.zero.values)) {
+            sig1 = rownames(metric.values)[non.zero.values[i, 1]]
+            sig2 = rownames(metric.values)[non.zero.values[i, 2]]
+            
+            p.values[sig1, sig2] = 1 - percentile(PCAWG.null.dists[[tissue]][[metric.name]][sig1, sig2, ],
+                                                  abs(metric.values[sig1, sig2]))
+        }
+        
+        if (!file.exists(out.file)) {
+            wb <- createWorkbook()
+            cat("File doesn't exist.\n")
+        } else { 
+            wb = loadWorkbook(out.file)
+        }
+        
+        if (!(tissue %in% names(wb))) {
+            cat("Sheet doesn't exist.\n")
+            addWorksheet(wb, tissue)
+            writeData(wb = wb, 
+                      sheet = tissue,
+                      x = metric.name)
+            start.row = 2
+        } else {
+            cat("Sheet exists.\n")
+            start.row = nrow(readWorkbook(out.file, 
+                                          sheet = tissue, 
+                                          colNames = FALSE, 
+                                          skipEmptyRows = FALSE))+5
+        }
+        
+        writeData(wb = wb, 
+                  sheet = tissue,
+                  x = metric.name,
+                  colNames = TRUE,
+                  startRow = start.row - 1,
+                  startCol = 1) 
+        cat("File exists.\n")
+        
+        
+        cat(metric.name, "\t", start.row, "\n") 
+        
+        ### Write the metric values
+        writeData(wb = wb, 
+                  sheet = tissue,
+                  x = metric.values,
+                  colNames = TRUE,
+                  rowNames = TRUE,
+                  startRow = start.row)
+        
+        ### Write the p-values annotation
+        writeData(wb = wb, 
+                  sheet = tissue,
+                  x = "p-values",
+                  colNames = TRUE,
+                  startRow = start.row - 1,
+                  startCol = ncol(metric.values)+4) 
+        
+        ### Write the p-values matrix
+        
+        p.values [upper.tri(p.values)] =  ## replace NA's with 1 in upper.tri
+            pmin(1, p.values[upper.tri(p.values)], na.rm = TRUE)
+        
+        writeData(wb = wb, 
+                  sheet = tissue,
+                  x = p.values,
+                  colNames = TRUE,
+                  rowNames = TRUE,
+                  startRow = start.row,
+                  startCol = ncol(metric.values)+4)
+        
+        saveWorkbook(wb, file = out.file, overwrite = TRUE)
+        
+    }
+    
+}
+
+
+saveRDS(all.sig.interactions, file = here("data/RDS/PCAWG/signatures/PCAWG.all.significant.interactions.RDS"))
--- R/sig_sig_interactions_TCGA.R
+++ R/sig_sig_interactions_TCGA.R
@@ -1,6 +1,7 @@
 library(here)
 
 source(here("R/load_packages.R"))
+library(openxlsx)
 
 fig.dir = here("figures/sig_sig_analysis")
 data.out.dir = here("data/RDS/TCGA", "sig_sig_ints")
@@ -13,6 +14,9 @@ for (dir.path in c(fig.dir, data.out.dir) ) {
 
 TCGA.full.subset.ann = readRDS(here("data", "RDS", "TCGA", "signatures",
                                      "TCGA.full.subset.ann.RDS"))
+
+TCGA.null.dists = readRDS(file.path(here("data", "RDS", "TCGA", "signatures"),
+                                      "TCGA.null.model.metrics.dist.RDS"))
 
 ### Cooccurrence
 
@@ -109,3 +113,148 @@ TCGA.coda.summary.nets = lapply(TCGA.coda, summarize_calcs )
 saveRDS(TCGA.coda.summary.nets, 
         file = file.path(data.out.dir, "TCGA.coda.summary.nets.RDS") )
 
+
+### Writing to supplements
+
+
+out.file = here("supp_data/tcga_sig_sig_all_metrics_report.xlsx")
+
+
+all.interactions = list(MI = TCGA.bcmi.summary.nets,
+                        cooccurrence = TCGA.cooccurrence.summary.nets,
+                        CoDa = TCGA.coda.summary.nets,
+                        Spearman = TCGA.spearman.summary.nets)
+
+tissues = unique(TCGA.full.subset.ann$Cancer.Types)
+
+sig.threshold = 0.05
+
+all.sig.interactions = all.interactions
+for(tissue in tissues) {
+    
+    for (metric.name in names(all.interactions)) {
+        cat(tissue, "\n")
+        out =  all.interactions[[metric.name]][[tissue]]
+        metric.values = out %>% rm_zeros()
+        if (length(metric.values) == 0) {next} 
+        
+        p.values = matrix(1, nrow = nrow(metric.values), 
+                          ncol = ncol(metric.values), 
+                          dimnames = (dimnames(metric.values)))
+        
+        metric.values[lower.tri(metric.values, diag = TRUE) ] = 0
+        
+        non.zero.values = which(metric.values != 0, arr.ind = TRUE)
+        
+        for(i in 1:nrow(non.zero.values)) {
+            sig1 = rownames(metric.values)[non.zero.values[i, 1]]
+            sig2 = rownames(metric.values)[non.zero.values[i, 2]]
+            
+            p.values[sig1, sig2] = 1 - percentile(TCGA.null.dists[[tissue]][[metric.name]][sig1, sig2, ],
+                                                  abs(metric.values[sig1, sig2]))
+            
+            if(p.values[sig1, sig2] > sig.threshold) {
+                out[sig1, sig2] = 0
+                out[sig2, sig1] = 0
+            }
+        }
+        all.sig.interactions[[metric.name]][[tissue]] = out
+    }
+}
+
+
+### Write the metric name
+for (tissue in tissues) {
+    for (metric.name in names(all.sig.interactions)) {
+        
+        cat(tissue, "\n")
+        metric.values = all.sig.interactions[[metric.name]][[tissue]] %>% rm_zeros()
+        
+        if (length(metric.values) == 0) {next} 
+        
+        p.values = matrix(NA, nrow = nrow(metric.values), 
+                          ncol = ncol(metric.values), 
+                          dimnames = (dimnames(metric.values)))
+        
+        metric.values[lower.tri(metric.values, diag = TRUE) ] = NA
+        
+        non.zero.values = which(metric.values != 0, arr.ind = TRUE)
+        
+        for(i in 1:nrow(non.zero.values)) {
+            sig1 = rownames(metric.values)[non.zero.values[i, 1]]
+            sig2 = rownames(metric.values)[non.zero.values[i, 2]]
+            
+            p.values[sig1, sig2] = 1 - percentile(TCGA.null.dists[[tissue]][[metric.name]][sig1, sig2, ],
+                                                  abs(metric.values[sig1, sig2]))
+        }
+        
+        if (!file.exists(out.file)) {
+            wb <- createWorkbook()
+            cat("File doesn't exist.\n")
+        } else { 
+            wb = loadWorkbook(out.file)
+        }
+        
+        if (!(tissue %in% names(wb))) {
+            cat("Sheet doesn't exist.\n")
+            addWorksheet(wb, tissue)
+            writeData(wb = wb, 
+                      sheet = tissue,
+                      x = metric.name)
+            start.row = 2
+        } else {
+            cat("Sheet exists.\n")
+            start.row = nrow(readWorkbook(out.file, 
+                                          sheet = tissue, 
+                                          colNames = FALSE, 
+                                          skipEmptyRows = FALSE))+5
+        }
+        
+        writeData(wb = wb, 
+                  sheet = tissue,
+                  x = metric.name,
+                  colNames = TRUE,
+                  startRow = start.row - 1,
+                  startCol = 1) 
+        cat("File exists.\n")
+        
+        
+        cat(metric.name, "\t", start.row, "\n") 
+        
+        ### Write the metric values
+        writeData(wb = wb, 
+                  sheet = tissue,
+                  x = metric.values,
+                  colNames = TRUE,
+                  rowNames = TRUE,
+                  startRow = start.row)
+        
+        ### Write the p-values annotation
+        writeData(wb = wb, 
+                  sheet = tissue,
+                  x = "p-values",
+                  colNames = TRUE,
+                  startRow = start.row - 1,
+                  startCol = ncol(metric.values)+4) 
+        
+        ### Write the p-values matrix
+        
+        p.values [upper.tri(p.values)] =  ## replace NA's with 1 in upper.tri
+            pmin(1, p.values[upper.tri(p.values)], na.rm = TRUE)
+        
+        writeData(wb = wb, 
+                  sheet = tissue,
+                  x = p.values,
+                  colNames = TRUE,
+                  rowNames = TRUE,
+                  startRow = start.row,
+                  startCol = ncol(metric.values)+4)
+        
+        saveWorkbook(wb, file = out.file, overwrite = TRUE)
+        
+    }
+    
+}
+
+
+saveRDS(all.sig.interactions, file = here("data/RDS/TCGA/signatures/TCGA.all.significant.interactions.RDS"))
