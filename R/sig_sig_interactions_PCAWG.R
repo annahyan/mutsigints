20e46b8b5a3680b30805f5b5a346bd4cf695ac51
annahyan <ahakobyan@cemm.oeaw.ac.at>
2023-11-24 22:41:41 +0100

    fixed the p-value calculation

--- R/sig_sig_interactions_PCAWG.R
+++ R/sig_sig_interactions_PCAWG.R
@@ -17,107 +17,114 @@ PCAWG.full.subset.ann = readRDS(here("data", "RDS", "PCAWG", "signatures",
 PCAWG.null.dists = readRDS(file.path(here("data", "RDS", "PCAWG", "signatures"),
                                       "PCAWG.null.model.metrics.dist.RDS"))
 
+tissues = names(PCAWG.null.dists)
 ### Cooccurrence
 
-PCAWG.cooccurrence = get_metrics_list(PCAWG.full.subset.ann, cooccurrence,
-                 min.tissue.samples = 20,
-                 sample.rate = 0.9, sample.N = 100, 
-                 N = 1, seed = 1, p.adjust = TRUE)
+# PCAWG.cooccurrence = get_metrics_list(PCAWG.full.subset.ann, cooccurrence,
+#                  min.tissue.samples = 20,
+#                  sample.rate = 0.9, sample.N = 100, 
+#                  N = 1, seed = 1, p.adjust = TRUE)
+# 
+# saveRDS(PCAWG.cooccurrence, 
+#         file = file.path(data.out.dir, "PCAWG.cooccurrence.RDS") )
+# 
+# # Plotting individual experiments
+# # pp = plot_all_experiments(PCAWG.cooccurrence$Biliary_AdenoCA, "title")
+# 
+# PCAWG.cooccurrence.summary.nets = lapply(PCAWG.cooccurrence, summarize_calcs )
+# 
+# saveRDS(PCAWG.cooccurrence.summary.nets, 
+#         file = file.path(data.out.dir, "PCAWG.cooccurrence.summary.nets.RDS") )
+# 
+# ### Mutual information(BCMI)
+# 
+# PCAWG.bcmi = get_metrics_list(PCAWG.full.subset.ann, bcmi,
+#                                       min.tissue.samples = 20,
+#                                       sample.rate = 0.9, sample.N = 100, 
+#                                       N = 1, seed = 1, p.adjust = TRUE)
+# 
+# saveRDS(PCAWG.bcmi, 
+#         file = file.path(data.out.dir, "PCAWG.bcmi.RDS") )
+# 
+# #Plotting individual experiments
+# # pp = plot_all_experiments(PCAWG.bcmi$Biliary_AdenoCA, "title")
+# 
+# PCAWG.bcmi.summary.nets = lapply(PCAWG.bcmi, summarize_calcs )
+# 
+# saveRDS(PCAWG.bcmi.summary.nets, 
+#         file = file.path(data.out.dir, "PCAWG.bcmi.summary.nets.RDS") )
 
-saveRDS(PCAWG.cooccurrence, 
-        file = file.path(data.out.dir, "PCAWG.cooccurrence.RDS") )
-
-# Plotting individual experiments
-# pp = plot_all_experiments(PCAWG.cooccurrence$Biliary_AdenoCA, "title")
-
-PCAWG.cooccurrence.summary.nets = lapply(PCAWG.cooccurrence, summarize_calcs )
-
-saveRDS(PCAWG.cooccurrence.summary.nets, 
-        file = file.path(data.out.dir, "PCAWG.cooccurrence.summary.nets.RDS") )
-
-### Mutual information(BCMI)
-
-PCAWG.bcmi = get_metrics_list(PCAWG.full.subset.ann, bcmi,
-                                      min.tissue.samples = 20,
-                                      sample.rate = 0.9, sample.N = 100, 
-                                      N = 1, seed = 1, p.adjust = TRUE)
-
-saveRDS(PCAWG.bcmi, 
-        file = file.path(data.out.dir, "PCAWG.bcmi.RDS") )
-
-#Plotting individual experiments
-# pp = plot_all_experiments(PCAWG.bcmi$Biliary_AdenoCA, "title")
-
-PCAWG.bcmi.summary.nets = lapply(PCAWG.bcmi, summarize_calcs )
-
-saveRDS(PCAWG.bcmi.summary.nets, 
-        file = file.path(data.out.dir, "PCAWG.bcmi.summary.nets.RDS") )
-
-### Pearson correlation
-
-PCAWG.pearson = get_metrics_list(PCAWG.full.subset.ann, cor_sigs,
-                              min.tissue.samples = 20,
-                              sample.rate = 0.9, sample.N = 100, 
-                              N = 1, seed = 1, p.adjust = TRUE,
-                              method = "pearson")
-
-saveRDS(PCAWG.pearson, 
-        file = file.path(data.out.dir, "PCAWG.pearson.RDS") )
-
-#Plotting individual experiments
-# pp = plot_all_experiments(PCAWG.pearson$Biliary_AdenoCA, "title")
-
-PCAWG.pearson.summary.nets = lapply(PCAWG.pearson, summarize_calcs )
-
-saveRDS(PCAWG.pearson.summary.nets, 
-        file = file.path(data.out.dir, "PCAWG.pearson.summary.nets.RDS") )
-
-
-### Spearman correlation
-
-PCAWG.spearman = get_metrics_list(PCAWG.full.subset.ann, cor_sigs,
-                                 min.tissue.samples = 20,
-                                 sample.rate = 0.9, sample.N = 100, 
-                                 N = 1, seed = 1, p.adjust = TRUE,
-                                 method = "spearman")
-
-saveRDS(PCAWG.spearman, 
-        file = file.path(data.out.dir, "PCAWG.spearman.RDS") )
-
-#Plotting individual experiments
-# pp = plot_all_experiments(PCAWG.spearman$Biliary_AdenoCA, "title")
-
-PCAWG.spearman.summary.nets = lapply(PCAWG.spearman, summarize_calcs )
-
-saveRDS(PCAWG.spearman.summary.nets, 
-        file = file.path(data.out.dir, "PCAWG.spearman.summary.nets.RDS") )
-
-
-### CoDa correlation
-
-PCAWG.coda = get_metrics_list(PCAWG.full.subset.ann, cor_coda,
-                                  min.tissue.samples = 20,
-                                  sample.rate = 0.9, sample.N = 100, 
-                                  N = 1, seed = 1, p.adjust = TRUE,
-                                  rand.add = FALSE)
-
-saveRDS(PCAWG.coda, 
-        file = file.path(data.out.dir, "PCAWG.coda.RDS") )
-
-#Plotting individual experiments
-# pp = plot_all_experiments(PCAWG.coda$Biliary_AdenoCA, "title")
-
-PCAWG.coda.summary.nets = lapply(PCAWG.coda, summarize_calcs )
-
-saveRDS(PCAWG.coda.summary.nets, 
-        file = file.path(data.out.dir, "PCAWG.coda.summary.nets.RDS") )
+# ### Pearson correlation
+# 
+# PCAWG.pearson = get_metrics_list(PCAWG.full.subset.ann, cor_sigs,
+#                               min.tissue.samples = 20,
+#                               sample.rate = 0.9, sample.N = 100, 
+#                               N = 1, seed = 1, p.adjust = TRUE,
+#                               method = "pearson")
+# 
+# saveRDS(PCAWG.pearson, 
+#         file = file.path(data.out.dir, "PCAWG.pearson.RDS") )
+# 
+# #Plotting individual experiments
+# # pp = plot_all_experiments(PCAWG.pearson$Biliary_AdenoCA, "title")
+# 
+# PCAWG.pearson.summary.nets = lapply(PCAWG.pearson, summarize_calcs )
+# 
+# saveRDS(PCAWG.pearson.summary.nets, 
+#         file = file.path(data.out.dir, "PCAWG.pearson.summary.nets.RDS") )
+# 
+# 
+# ### Spearman correlation
+# 
+# PCAWG.spearman = get_metrics_list(PCAWG.full.subset.ann, cor_sigs,
+#                                  min.tissue.samples = 20,
+#                                  sample.rate = 0.9, sample.N = 100, 
+#                                  N = 1, seed = 1, p.adjust = TRUE,
+#                                  method = "spearman")
+# 
+# saveRDS(PCAWG.spearman, 
+#         file = file.path(data.out.dir, "PCAWG.spearman.RDS") )
+# 
+# #Plotting individual experiments
+# # pp = plot_all_experiments(PCAWG.spearman$Biliary_AdenoCA, "title")
+# 
+# PCAWG.spearman.summary.nets = lapply(PCAWG.spearman, summarize_calcs )
+# 
+# saveRDS(PCAWG.spearman.summary.nets, 
+#         file = file.path(data.out.dir, "PCAWG.spearman.summary.nets.RDS") )
+# 
+# 
+# ### CoDa correlation
+# 
+# PCAWG.coda = get_metrics_list(PCAWG.full.subset.ann, cor_coda,
+#                                   min.tissue.samples = 20,
+#                                   sample.rate = 0.9, sample.N = 100, 
+#                                   N = 1, seed = 1, p.adjust = TRUE,
+#                                   rand.add = FALSE)
+# 
+# saveRDS(PCAWG.coda, 
+#         file = file.path(data.out.dir, "PCAWG.coda.RDS") )
+# 
+# #Plotting individual experiments
+# # pp = plot_all_experiments(PCAWG.coda$Biliary_AdenoCA, "title")
+# 
+# PCAWG.coda.summary.nets = lapply(PCAWG.coda, summarize_calcs )
+# 
+# saveRDS(PCAWG.coda.summary.nets, 
+#         file = file.path(data.out.dir, "PCAWG.coda.summary.nets.RDS") )
 
 
 ### Writing to supplements
+PCAWG.cooccurrence.summary.nets = readRDS(file = file.path(data.out.dir, "PCAWG.cooccurrence.summary.nets.RDS") )
 
+PCAWG.bcmi.summary.nets = readRDS(file = file.path(data.out.dir, "PCAWG.bcmi.summary.nets.RDS") )
+
+PCAWG.spearman.summary.nets = readRDS(file = file.path(data.out.dir, "PCAWG.spearman.summary.nets.RDS") )
+
+PCAWG.coda.summary.nets = readRDS(file = file.path(data.out.dir, "PCAWG.coda.summary.nets.RDS") )
 
 out.file = here("supp_data/pcawg_sig_sig_all_metrics_report.xlsx")
-
+file.remove(out.file)
 
 all.interactions = list(MI = PCAWG.bcmi.summary.nets,
                         cooccurrence = PCAWG.cooccurrence.summary.nets,
@@ -148,8 +155,10 @@ for(tissue in tissues) {
             sig1 = rownames(metric.values)[non.zero.values[i, 1]]
             sig2 = rownames(metric.values)[non.zero.values[i, 2]]
             
-            p.values[sig1, sig2] = 1 - percentile(PCAWG.null.dists[[tissue]][[metric.name]][sig1, sig2, ],
-                                                  abs(metric.values[sig1, sig2]))
+            p.values[sig1, sig2] = p.values[sig1, sig2] = min(percentile(PCAWG.null.dists[[tissue]][[metric.name]][sig1, sig2, ],
+                                                                         abs(metric.values[sig1, sig2])),
+                                                              1 - percentile(PCAWG.null.dists[[tissue]][[metric.name]][sig1, sig2, ],
+                                                                             abs(metric.values[sig1, sig2]))  )
             
             if(p.values[sig1, sig2] > sig.threshold) {
                 out[sig1, sig2] = 0
@@ -182,8 +191,10 @@ for (tissue in tissues) {
             sig1 = rownames(metric.values)[non.zero.values[i, 1]]
             sig2 = rownames(metric.values)[non.zero.values[i, 2]]
             
-            p.values[sig1, sig2] = 1 - percentile(PCAWG.null.dists[[tissue]][[metric.name]][sig1, sig2, ],
-                                                  abs(metric.values[sig1, sig2]))
+            p.values[sig1, sig2] = min(percentile(PCAWG.null.dists[[tissue]][[metric.name]][sig1, sig2, ],
+                                                  abs(metric.values[sig1, sig2])),
+                                       1 - percentile(PCAWG.null.dists[[tissue]][[metric.name]][sig1, sig2, ],
+                                                      abs(metric.values[sig1, sig2]))  )
         }
         
         if (!file.exists(out.file)) {
@@ -251,7 +262,6 @@ for (tissue in tissues) {
         saveWorkbook(wb, file = out.file, overwrite = TRUE)
         
     }
-    
 }
 
 
--- R/sig_sig_interactions_TCGA.R
+++ R/sig_sig_interactions_TCGA.R
@@ -17,107 +17,115 @@ TCGA.full.subset.ann = readRDS(here("data", "RDS", "TCGA", "signatures",
 
 TCGA.null.dists = readRDS(file.path(here("data", "RDS", "TCGA", "signatures"),
                                       "TCGA.null.model.metrics.dist.RDS"))
-
-### Cooccurrence
-
-TCGA.cooccurrence = get_metrics_list(TCGA.full.subset.ann, cooccurrence,
-                 min.tissue.samples = 20,
-                 sample.rate = 0.9, sample.N = 100, 
-                 N = 1, seed = 1, p.adjust = TRUE)
-
-saveRDS(TCGA.cooccurrence, 
-        file = file.path(data.out.dir, "TCGA.cooccurrence.RDS") )
-
-# Plotting individual experiments
-# pp = plot_all_experiments(TCGA.cooccurrence$Biliary_AdenoCA, "title")
-
-TCGA.cooccurrence.summary.nets = lapply(TCGA.cooccurrence, summarize_calcs )
-
-saveRDS(TCGA.cooccurrence.summary.nets, 
-        file = file.path(data.out.dir, "TCGA.cooccurrence.summary.nets.RDS") )
-
-### Mutual information(BCMI)
-
-TCGA.bcmi = get_metrics_list(TCGA.full.subset.ann, bcmi,
-                                      min.tissue.samples = 20,
-                                      sample.rate = 0.9, sample.N = 100, 
-                                      N = 1, seed = 1, p.adjust = TRUE)
-
-saveRDS(TCGA.bcmi, 
-        file = file.path(data.out.dir, "TCGA.bcmi.RDS") )
-
-#Plotting individual experiments
-# pp = plot_all_experiments(TCGA.bcmi$Biliary_AdenoCA, "title")
-
-TCGA.bcmi.summary.nets = lapply(TCGA.bcmi, summarize_calcs )
-
-saveRDS(TCGA.bcmi.summary.nets, 
-        file = file.path(data.out.dir, "TCGA.bcmi.summary.nets.RDS") )
-
-### Pearson correlation
-
-TCGA.pearson = get_metrics_list(TCGA.full.subset.ann, cor_sigs,
-                              min.tissue.samples = 20,
-                              sample.rate = 0.9, sample.N = 100, 
-                              N = 1, seed = 1, p.adjust = TRUE,
-                              method = "pearson")
-
-saveRDS(TCGA.pearson, 
-        file = file.path(data.out.dir, "TCGA.pearson.RDS") )
-
-#Plotting individual experiments
-# pp = plot_all_experiments(TCGA.pearson$Biliary_AdenoCA, "title")
-
-TCGA.pearson.summary.nets = lapply(TCGA.pearson, summarize_calcs )
-
-saveRDS(TCGA.pearson.summary.nets, 
-        file = file.path(data.out.dir, "TCGA.pearson.summary.nets.RDS") )
-
-
-### Spearman correlation
-
-TCGA.spearman = get_metrics_list(TCGA.full.subset.ann, cor_sigs,
-                                 min.tissue.samples = 20,
-                                 sample.rate = 0.9, sample.N = 100, 
-                                 N = 1, seed = 1, p.adjust = TRUE,
-                                 method = "spearman")
-
-saveRDS(TCGA.spearman, 
-        file = file.path(data.out.dir, "TCGA.spearman.RDS") )
-
-#Plotting individual experiments
-# pp = plot_all_experiments(TCGA.spearman$Biliary_AdenoCA, "title")
-
-TCGA.spearman.summary.nets = lapply(TCGA.spearman, summarize_calcs )
-
-saveRDS(TCGA.spearman.summary.nets, 
-        file = file.path(data.out.dir, "TCGA.spearman.summary.nets.RDS") )
-
-
-### CoDa correlation
-
-TCGA.coda = get_metrics_list(TCGA.full.subset.ann, cor_coda,
-                                  min.tissue.samples = 20,
-                                  sample.rate = 0.9, sample.N = 100, 
-                                  N = 1, seed = 1, p.adjust = TRUE,
-                                  rand.add = FALSE)
-
-saveRDS(TCGA.coda, 
-        file = file.path(data.out.dir, "TCGA.coda.RDS") )
-
-#Plotting individual experiments
-# pp = plot_all_experiments(TCGA.coda$Biliary_AdenoCA, "title")
-
-TCGA.coda.summary.nets = lapply(TCGA.coda, summarize_calcs )
-
-saveRDS(TCGA.coda.summary.nets, 
-        file = file.path(data.out.dir, "TCGA.coda.summary.nets.RDS") )
+tissues = names(TCGA.null.dists)
+# ### Cooccurrence
+# 
+# TCGA.cooccurrence = get_metrics_list(TCGA.full.subset.ann, cooccurrence,
+#                  min.tissue.samples = 20,
+#                  sample.rate = 0.9, sample.N = 100, 
+#                  N = 1, seed = 1, p.adjust = TRUE)
+# 
+# saveRDS(TCGA.cooccurrence, 
+#         file = file.path(data.out.dir, "TCGA.cooccurrence.RDS") )
+# 
+# # Plotting individual experiments
+# # pp = plot_all_experiments(TCGA.cooccurrence$Biliary_AdenoCA, "title")
+# 
+# TCGA.cooccurrence.summary.nets = lapply(TCGA.cooccurrence, summarize_calcs )
+# 
+# saveRDS(TCGA.cooccurrence.summary.nets, 
+#         file = file.path(data.out.dir, "TCGA.cooccurrence.summary.nets.RDS") )
+# 
+# ### Mutual information(BCMI)
+# 
+# TCGA.bcmi = get_metrics_list(TCGA.full.subset.ann, bcmi,
+#                                       min.tissue.samples = 20,
+#                                       sample.rate = 0.9, sample.N = 100, 
+#                                       N = 1, seed = 1, p.adjust = TRUE)
+# 
+# saveRDS(TCGA.bcmi, 
+#         file = file.path(data.out.dir, "TCGA.bcmi.RDS") )
+# 
+# #Plotting individual experiments
+# # pp = plot_all_experiments(TCGA.bcmi$Biliary_AdenoCA, "title")
+# 
+# TCGA.bcmi.summary.nets = lapply(TCGA.bcmi, summarize_calcs )
+# 
+# saveRDS(TCGA.bcmi.summary.nets, 
+#         file = file.path(data.out.dir, "TCGA.bcmi.summary.nets.RDS") )
+# 
+# ### Pearson correlation
+# 
+# TCGA.pearson = get_metrics_list(TCGA.full.subset.ann, cor_sigs,
+#                               min.tissue.samples = 20,
+#                               sample.rate = 0.9, sample.N = 100, 
+#                               N = 1, seed = 1, p.adjust = TRUE,
+#                               method = "pearson")
+# 
+# saveRDS(TCGA.pearson, 
+#         file = file.path(data.out.dir, "TCGA.pearson.RDS") )
+# 
+# #Plotting individual experiments
+# # pp = plot_all_experiments(TCGA.pearson$Biliary_AdenoCA, "title")
+# 
+# TCGA.pearson.summary.nets = lapply(TCGA.pearson, summarize_calcs )
+# 
+# saveRDS(TCGA.pearson.summary.nets, 
+#         file = file.path(data.out.dir, "TCGA.pearson.summary.nets.RDS") )
+# 
+# 
+# ### Spearman correlation
+# 
+# TCGA.spearman = get_metrics_list(TCGA.full.subset.ann, cor_sigs,
+#                                  min.tissue.samples = 20,
+#                                  sample.rate = 0.9, sample.N = 100, 
+#                                  N = 1, seed = 1, p.adjust = TRUE,
+#                                  method = "spearman")
+# 
+# saveRDS(TCGA.spearman, 
+#         file = file.path(data.out.dir, "TCGA.spearman.RDS") )
+# 
+# #Plotting individual experiments
+# # pp = plot_all_experiments(TCGA.spearman$Biliary_AdenoCA, "title")
+# 
+# TCGA.spearman.summary.nets = lapply(TCGA.spearman, summarize_calcs )
+# 
+# saveRDS(TCGA.spearman.summary.nets, 
+#         file = file.path(data.out.dir, "TCGA.spearman.summary.nets.RDS") )
+# 
+# 
+# ### CoDa correlation
+# 
+# TCGA.coda = get_metrics_list(TCGA.full.subset.ann, cor_coda,
+#                                   min.tissue.samples = 20,
+#                                   sample.rate = 0.9, sample.N = 100, 
+#                                   N = 1, seed = 1, p.adjust = TRUE,
+#                                   rand.add = FALSE)
+# 
+# saveRDS(TCGA.coda, 
+#         file = file.path(data.out.dir, "TCGA.coda.RDS") )
+# 
+# #Plotting individual experiments
+# # pp = plot_all_experiments(TCGA.coda$Biliary_AdenoCA, "title")
+# 
+# TCGA.coda.summary.nets = lapply(TCGA.coda, summarize_calcs )
+# 
+# saveRDS(TCGA.coda.summary.nets, 
+#         file = file.path(data.out.dir, "TCGA.coda.summary.nets.RDS") )
 
 
 ### Writing to supplements
 
+TCGA.cooccurrence.summary.nets = readRDS(file = file.path(data.out.dir, "TCGA.cooccurrence.summary.nets.RDS") )
+
+TCGA.bcmi.summary.nets = readRDS(file = file.path(data.out.dir, "TCGA.bcmi.summary.nets.RDS") )
+
+TCGA.spearman.summary.nets = readRDS(file = file.path(data.out.dir, "TCGA.spearman.summary.nets.RDS") )
+
+TCGA.coda.summary.nets = readRDS(file = file.path(data.out.dir, "TCGA.coda.summary.nets.RDS") )
 
 out.file = here("supp_data/tcga_sig_sig_all_metrics_report.xlsx")
+file.remove(out.file)
 
 
 all.interactions = list(MI = TCGA.bcmi.summary.nets,
@@ -150,8 +158,10 @@ for(tissue in tissues) {
             sig1 = rownames(metric.values)[non.zero.values[i, 1]]
             sig2 = rownames(metric.values)[non.zero.values[i, 2]]
             
-            p.values[sig1, sig2] = 1 - percentile(TCGA.null.dists[[tissue]][[metric.name]][sig1, sig2, ],
-                                                  abs(metric.values[sig1, sig2]))
+            p.values[sig1, sig2] = min(percentile(TCGA.null.dists[[tissue]][[metric.name]][sig1, sig2, ],
+                                                  abs(metric.values[sig1, sig2])),
+                                       1 - percentile(TCGA.null.dists[[tissue]][[metric.name]][sig1, sig2, ],
+                                                      abs(metric.values[sig1, sig2]))  )
             
             if(p.values[sig1, sig2] > sig.threshold) {
                 out[sig1, sig2] = 0
@@ -184,8 +194,10 @@ for (tissue in tissues) {
             sig1 = rownames(metric.values)[non.zero.values[i, 1]]
             sig2 = rownames(metric.values)[non.zero.values[i, 2]]
             
-            p.values[sig1, sig2] = 1 - percentile(TCGA.null.dists[[tissue]][[metric.name]][sig1, sig2, ],
-                                                  abs(metric.values[sig1, sig2]))
+            p.values[sig1, sig2] = min(percentile(TCGA.null.dists[[tissue]][[metric.name]][sig1, sig2, ],
+                                                  abs(metric.values[sig1, sig2])),
+                                       1 - percentile(TCGA.null.dists[[tissue]][[metric.name]][sig1, sig2, ],
+                                                      abs(metric.values[sig1, sig2]))  )
         }
         
         if (!file.exists(out.file)) {
