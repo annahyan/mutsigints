b8516f98e454815239abc215dfa3b83d396d2c4a
annahyan <anna@menchelab.com>
2023-11-30 22:24:50 +0100

    debugging survival code

---
+++ analysis/debugging_the_survivals.R
@@ -0,0 +1,37 @@
+sig1 = "MMR"
+sig2 = "HIPPO"
+tissue = "Uterus_AdenoCa"
+tissues = tissue
+signatures = c(sig1, sig2)
+
+dataset = TCGA.path.sig.aggregate
+clin.df = TCGA.clin.df
+param.values = list(
+    "age.at.diagnosis" = c(TRUE, FALSE),
+    "with.total.muts" = c(TRUE, FALSE), 
+    "tmb.logged" = c(TRUE),
+    "binary.status" = c(FALSE),
+    "epistatic" = c(TRUE, FALSE))
+filename = here("supp_data/tcga_positive_sig_path_survivals.xlsx")
+min.sample.fraction = 0
+rm.non.sig.sheets = TRUE
+return.only.sig = TRUE
+
+
+
+oo = pick_survival_model_int(dataset,
+                                   tissue,
+                                   signatures,
+                                   clin.df,
+                                   param.values,
+                                   min.sample.fraction = min.sample.fraction,
+                                   filename = filename,
+                                   rm.non.sig.sheets = FALSE,
+                                   return.only.sig = FALSE)
+
+age.at.diagnosis = TRUE
+with.total.muts = TRUE
+tmb.logged = TRUE
+binary.status = FALSE
+epistatic = TRUE
+
